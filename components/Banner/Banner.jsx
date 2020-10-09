5ece0f37b7eb094c4902d7d99495d8aaf6a45d94
Bruno Paula <devbrunopaula@gmail.com>
2020-10-08 22:58:10 -0400

    add lets meet btn

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {BsArrowRight} from 'react-icons/bs'
 
 function Banner() {
   return (
     <>
-      <div className='banner'></div>
+      <div className='welcome'>
+        <h1>Hi, my name is Bruno</h1>
+        <div className='welcome-tagline'>
+          <p>
+            A RESPONSIVE SITE TEMPLATE DESIGNED BY HTML5 UP <br></br>AND
+            RELEASED UNDER THE CREATIVE COMMONS.
+          </p>
+          <div className='meet'>
+            <button className='meet-button'>LET'S MEET</button>
+            <BsArrowRight className='meet-arrowIcon' />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Banner from '../components/Banner/Banner'
 import Navigation from '../components/Nav/Navigation'
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       </Head>
       <div className='banner'>
         <Navigation />
+        <Banner />
       </div>
     </>
   )
