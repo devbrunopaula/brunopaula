d60c6282e20339d289da462f4f559204f125ed25
Bruno Paula <devbrunopaula@gmail.com>
2020-10-08 20:14:26 -0400

    test scss

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import Navigation from '../components/Nav/Navigation'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Bruno Paula | Portfolio</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <Navigation />
     </>
   )
