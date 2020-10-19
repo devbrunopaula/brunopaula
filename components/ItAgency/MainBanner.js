8c78b58dae486bfba405158a080704daaca1be7f
Bruno Paula <devbrunopaula@gmail.com>
2020-10-18 21:00:16 -0400

    test nav

---
+++ components/ItAgency/MainBanner.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react'
+import Link from 'next/link'
+import ModalVideo from 'react-modal-video'
+import Typist from 'react-typist'
+
+class MainBanner extends Component {
+  state = {
+    isOpen: false,
+  }
+  openModal = () => {
+    this.setState({isOpen: true})
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className='hero-banner it-banner overly hero-bg4'>
+          <div className='d-table'>
+            <div className='d-table-cell'>
+              <div className='container'>
+                <div className='row align-items-center'>
+                  <div className='col-lg-6'>
+                    <div className='main-banner-content'>
+                      <Typist>
+                        <h1>Hi, I am Bruno</h1>
+
+                        <p>I am a Full-Stack Web Developer</p>
+                      </Typist>
+                      <Link href='/contact'>
+                        <a className='btn btn-primary'>Let's Meet</a>
+                      </Link>
+
+                      <Link href='/portfolio'>
+                        {/* <a
+                          // onClick={(e) => {
+                          //   e.preventDefault()
+                          //   this.openModal()
+                          // }}
+                          className='btn btn-secondary'
+                        ></a> */}
+                        <a className='btn btn-secondary'>
+                          <i className='icofont-ui-play'></i> View my work
+                        </a>
+                      </Link>
+                    </div>
+                  </div>
+
+                  <div className='col-lg-6'>
+                    <div className='animate-banner-image'>
+                      <img src='/images/brunopaula.jpg' alt='Animate image' />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default MainBanner
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,43 @@
-import Head from 'next/head'
-import Banner from '../components/Banner/Banner'
-import Navigation from '../components/Nav/Navigation'
+import React, {Component} from 'react'
+import Navbar from '../components/Layouts/NavbarTwo'
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Bruno Paula | Portfolio</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <div className='banner'>
-        <Navigation />
-        <Banner />
-      </div>
-    </>
-  )
+import MainBanner from '../components/ItAgency/MainBanner'
+import OurServices from '../components/ItAgency/OurServices'
+import ServiceStyleTwo from '../components/ItAgency/ServiceStyleTwo'
+import WhyChooseUs from '../components/ItAgency/WhyChooseUs'
+import FunFactsTwo from '../components/Common/FunFactsTwo'
+import Feedback from '../components/Common/Feedback'
+import PricingStyleThree from '../components/Pricing/PricingStyleThree'
+import TeamStyleTwo from '../components/Common/TeamStyleTwo'
+import LatestNewsSliderTwo from '../components/Common/LatestNewsSliderTwo'
+import Newsletter from '../components/Common/Newsletter'
+import Footer from '../components/Layouts/Footer'
+import Partner from '../components/Common/Partner'
+
+class ItAgency extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+
+        <MainBanner />
+        <OurServices />
+        <ServiceStyleTwo />
+        <WhyChooseUs />
+        <FunFactsTwo />
+        <Partner />
+        <Feedback />
+        <TeamStyleTwo />
+        <PricingStyleThree />
+
+        <LatestNewsSliderTwo />
+        <div className='pb-100'>
+          <Newsletter />
+        </div>
+        <Footer />
+      </React.Fragment>
+    )
+  }
 }
+
+export default ItAgency
