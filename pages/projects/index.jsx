678fc28c5d1482e5d764ddfc20bb81612d58fc93
Bruno Paula <devbrunopaula@gmail.com>
2021-07-21 00:01:09 -0400

    add more projects

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -28,14 +28,13 @@ const Projects = ({data}) => {
 export default Projects
 
 export async function getStaticProps(contex) {
-	const client = createClient({
+	const client = await createClient({
 		space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
 		accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
 	})
 
 	const result = await client.getEntries({
 		content_type: 'projects',
-		// limit: 2,
 	})
 
 	return {
